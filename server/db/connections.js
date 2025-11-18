c92fbaca9e24ad0955248018679eb26720ba7511
tonysodeep <hynguyen2727@gmail.com>
2025-11-18 14:35:52 +0700

    Remove unused mirgate

--- server/db/connections.js
+++ server/db/connections.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log(`connection create successful`);
   } catch (error) {
