1f5b092a017d994565d4ac77d18d69fecaa6b6d1
tonysodeep <hynguyen2727@gmail.com>
2025-11-19 16:02:24 +0700

    update naming authController file

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login } from "../controllers/AuthController.js";
+import { login } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
